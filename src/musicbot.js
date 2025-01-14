310bbf227418b337e1c45db5af349e7a469c8917
nullpat <ppatkowski@null.net>
2025-01-14 08:05:33 -0500

    fixed TrackEndEvent and moved back to raw

--- src/musicbot.js
+++ src/musicbot.js
@@ -1,11 +1,12 @@
-import { Client, Collection, EmbedBuilder, GatewayIntentBits } from "discord.js";
+import {
+  Client,
+  Collection,
+  EmbedBuilder,
+  GatewayIntentBits,
+} from "discord.js";
 import { readdirSync } from "fs";
-import { lavaClient } from "./events/raw.js";
-import { logger } from "./utils/logger.js";
-import { getVoice } from "./services/services.js";
 
 const eventFiles = readdirSync("./src/events");
-
 
 const client = new Client({
   intents: [
@@ -26,38 +27,5 @@ for (const eventFile of eventFiles) {
     }
   });
 }
-
-lavaClient.on("raw", async (data) => {
-  console.log(data);
-  if (data.type !== "TrackStartEvent") return;
-
-  try {
-    const voiceData = await getVoice(data.guildId);
-    const channel = client.channels.cache.get(voiceData.channelId);
-
-    if (!channel || !channel.isTextBased()) {
-      console.error(
-        `Channel Id must exist and allow text: ${voiceData.channelId}`
-      );
-      return;
-    }
-    const { title, author, uri, artworkUrl } = data.track.info;
-    const albumName = data.track.pluginInfo.albumName || "";
-
-    const nowPlaying = new EmbedBuilder()
-      .setTitle("Now Playing")
-      .setURL(uri)
-      .setThumbnail(artworkUrl)
-      .setDescription(
-        `**${title}**
-        ${author}
-        ${albumName}`
-      );
-
-    channel.send({ embeds: [nowPlaying] });
-  } catch (error) {
-    logger.error(error.stack);
-  }
-});
 
 export { client };
