d2854968d1f3d42896816ef6fc588bda2a01acf6
nullpat <ppatkowski@null.net>
2025-01-19 20:31:43 -0500

    don't attempt to join channel on every play if you dont need to

--- src/commands/move.js
+++ src/commands/move.js
@@ -5,22 +5,22 @@ import { joinChannel } from "../services/services.js";
 
 const data = new SlashCommandBuilder().setName("move").setDescription("Moves to your current voice channel");
 
-async function execute(interaction, message) {
+const execute = async (interaction, message) => {
   const guildId = message ? message.guildId : interaction.guildId;
   const channelId = message ? message.member.voice.channel.id : interaction.member.voice.channel.id;
 
   if (!channelId) {
-    sendMessage(interaction, message, "You are not in a voice channel.");
+    await sendMessage(interaction, message, "You are not in a voice channel.");
     return;
   }
 
   try {
     const join = await joinChannel(guildId, channelId);
-    sendMessage(interaction, message, join);
+    await sendMessage(interaction, message, join);
   } catch (error) {
     logger.error(error.stack);
-    sendMessage(interaction, message, error.message);
+    await sendMessage(interaction, message, error.message);
   }
-}
+};
 
 export { data, execute };
--- src/commands/play.js
+++ src/commands/play.js
@@ -1,3 +1,4 @@
+import FastLink from "@performanc/fastlink";
 import { SlashCommandBuilder, ButtonBuilder, ButtonStyle, ActionRowBuilder } from "discord.js";
 import { logger } from "../utils/logger.js";
 import { sendMessage, editMessage } from "../helpers/helpers.js";
@@ -9,24 +10,26 @@ const data = new SlashCommandBuilder()
   .setDescription("Adds a song to the queue, via URL or search (Shortcut is >> not >p because of pause command)")
   .addStringOption((option) => option.setName("song").setDescription("Enter song URL or search").setRequired(true));
 
-async function execute(interaction, message, messageInput, isYT) {
+const execute = async (interaction, message, messageInput, isYT) => {
   const songInput = message ? messageInput : interaction.options.getString("song");
   const guildId = message ? message.guildId : interaction.guildId;
   const channelId = message ? message.member.voice.channel?.id : interaction.member.voice.channel?.id;
-  const undoButton = new ButtonBuilder().setCustomId("undo").setLabel("Undo").setStyle(ButtonStyle.Secondary);
-  const row = isFirstStartEvent ? null : new ActionRowBuilder().addComponents(undoButton);
 
-  if (!channelId) {
-    sendMessage(interaction, message, "You are not in a voice channel.");
-    return;
-  }
+  const undoButton = new ButtonBuilder().setCustomId("undo").setLabel("Undo").setStyle(ButtonStyle.Secondary);
+  const actionRow = isFirstStartEvent ? null : new ActionRowBuilder().addComponents(undoButton);
 
   isYT = isYT ?? interaction.commandName === "youtube";
 
   try {
-    await joinChannel(guildId, channelId);
+    const player = new FastLink.player.Player(guildId);
+    if (!player.playerCreated()) {
+      if (!channelId) {
+        return await sendMessage(interaction, message, "You are not in a voice channel.");
+      }
+      await joinChannel(guildId, channelId);
+    }
     const play = await addSong(guildId, songInput, isYT);
-    const response = await sendMessage(interaction, message, play, row);
+    const response = await sendMessage(interaction, message, play, actionRow);
 
     if (isFirstStartEvent) {
       nowPlaying(guildId, isFirstStartEvent);
@@ -40,6 +43,6 @@ async function execute(interaction, message, messageInput, isYT) {
     logger.error(error.stack);
     sendMessage(interaction, message, error.message);
   }
-}
+};
 
 export { data, execute };
