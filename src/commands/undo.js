a2125ebc01168aebeba887f2e46deb8813dba199
nullpat <ppatkowski@null.net>
2026-01-26 22:11:08 -0800

    refactor undo to not give headache

--- src/commands/undo.js
+++ src/commands/undo.js
@@ -9,40 +9,50 @@ const data = new SlashCommandBuilder()
 
 const execute = async (interaction, message) => {
   const guildId = message?.guildId ?? interaction.guildId;
-  const userId = message?.author.id ?? interaction.user.id;
-  const cancelButton = new ButtonBuilder().setCustomId("cancel").setLabel("Cancel").setStyle(ButtonStyle.Primary);
-  const removeButton = new ButtonBuilder().setCustomId("remove").setLabel("Remove").setStyle(ButtonStyle.Danger);
-  const row = new ActionRowBuilder().addComponents(cancelButton, removeButton);
-  let confirmation;
+  const userId = message?.author?.id ?? interaction.user.id;
 
-  const handleConfirmation = async (interaction, response, userId, guildId) => {
-    const collectorFilter = (i) => i.user.id === userId;
+  const row = new ActionRowBuilder().addComponents(
+    new ButtonBuilder().setCustomId("cancel").setLabel("Cancel").setStyle(ButtonStyle.Primary),
+    new ButtonBuilder().setCustomId("remove").setLabel("Remove").setStyle(ButtonStyle.Danger),
+  );
 
+  let response;
+
+  try {
+    const last = checkLast(guildId);
+
+    response = await sendMessage(interaction, message, last, null, row, true);
+
+    let confirmation;
     try {
-      confirmation = await getComponent(response, collectorFilter, 15_000);
+      confirmation = await getComponent(response, (i) => i.user.id === userId, 15_000);
     } catch {
       await editMessage(interaction, response, "Undo confirmation not received", null, "");
+    }
+
+    if (confirmation.customId === "cancel") {
+      await confirmation.update({
+        content: "Undo cancelled",
+        components: [],
+      });
     }
 
     if (confirmation.customId === "remove") {
       const removeResultMsg = removeLast(guildId);
 
-      await confirmation.update({ content: removeResultMsg, components: [] });
-    } else if (confirmation.customId === "cancel") {
-      await confirmation.update({ content: "Undo cancelled", components: [] });
+      await confirmation.update({
+        content: removeResultMsg,
+        components: [],
+      });
     }
-  };
-
-  try {
-    const last = checkLast(guildId);
-    const response = await sendMessage(interaction, message, last, null, row, true);
-    await handleConfirmation(interaction, response, userId, guildId);
   } catch (error) {
     logger.error(error.stack);
-    await confirmation.update({
-      content: process.env.NODE_ENV !== "production" ? error.message : "Internal Server Error",
-      components: [],
-    });
+    const content = process.env.NODE_ENV !== "production" ? error.message : "Internal Server Error";
+    if (response) {
+      await editMessage(interaction, response, content, null, "");
+    } else {
+      await sendMessage(interaction, message, content);
+    }
   }
 };
 
